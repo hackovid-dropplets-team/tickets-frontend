be65c2c4ca343ea6c1a590bbbc34530c3021e6ae
Arnau Comas Codina <comas1992@gmail.com>
2020-04-08 17:33:36 +0200

    [feat] auth service

---
+++ react-app/src/api-services/auth.js
@@ -0,0 +1,33 @@
+import config from '../config.js';
+
+class AuthApiService {
+  static getAuth() {
+    return fetch(config.api.URL + "/auth", {
+      credentials: "include",
+    })
+    .then(res => res.json())
+  }
+
+  static postAuthLogin(username, password) {
+    return fetch(config.api.URL + "/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        "username": username,
+        "password": password
+      }),
+    })
+    .then(res => res.json())
+  }
+
+  static getAuthLogout() {
+    return fetch(config.api.URL + "/auth/logout", {
+      credentials: "include",
+    })
+  }
+}
+
+export default AuthApiService;
