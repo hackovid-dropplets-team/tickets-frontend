b051b986aaa0ceb3dea589bbb59d8af3149286f2
Arnau Comas Codina <comas1992@gmail.com>
2020-04-08 11:49:55 +0200

    [feat] updated start/build scripts for dev/prod

---
+++ react-app/src/config.js
@@ -0,0 +1,21 @@
+const dev = {
+  api: {
+    URL: "http://localhost:8000"
+  }
+};
+
+const prod = {
+  api: {
+    URL: window.location.protocol + "//" + window.location.hostname + (window.location.protocol === "http:" ? ":8000" : ":8443")
+  }
+};
+
+const config = process.env.REACT_APP_STAGE === 'production'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here. Example:
+  //MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
+};
