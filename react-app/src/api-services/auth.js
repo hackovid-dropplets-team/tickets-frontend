acd29b404174dc8a013c9d5e53a0089859b23566
Arnau Comas Codina <comas1992@gmail.com>
2020-04-09 16:20:55 +0200

    [fix] api services

--- react-app/src/api-services/auth.js
+++ react-app/src/api-services/auth.js
@@ -1,19 +1,26 @@
 import config from '../config.js';
-import BaseApiService from './base-api.js';
 
 class AuthApiService  {
-  static getAuth() { 
+  static getAuth() {
     return fetch(config.api.URL + "/auth", {
-      // credentials: "include",
+      credentials: "include",
     })
     .then(res => res.json())
   }
 
   static postAuthLogin(username, password) {
-    return BaseApiService.post( "login", JSON.stringify({
-      "username": username,
-      "password": password
-    })).then(res => {console.log(res);});
+    return fetch(config.api.URL + "/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        "username": username,
+        "password": password
+      }),
+    })
+    .then(res => res.json())
   }
 
   static postAuthRegister(username, password) {
