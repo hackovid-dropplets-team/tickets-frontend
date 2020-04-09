c015e6436f62c3a02c5477cee7972a296011b970
Arnau Comas Codina <comas1992@gmail.com>
2020-04-09 16:22:38 +0200

    [feat] tickets api service

---
+++ react-app/src/api-services/tickets.js
@@ -0,0 +1,32 @@
+import config from '../config.js';
+
+class TicketsApiService {
+  static getTickets() {
+    return fetch(config.api.URL + "/tickets", {
+      credentials: "include",
+    })
+    .then(res => res.json())
+  }
+
+  static postTickets(obj) {
+    return fetch(config.api.URL + "/tickets", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(obj),
+    })
+    .then(res => res.json())
+  }
+
+  static deleteTickets(id) {
+    return fetch(config.api.URL + "/tickets/" + id, {
+      method: "DELETE",
+      credentials: "include",
+    })
+    .then(res => res);
+  }
+}
+
+export default TicketsApiService;
