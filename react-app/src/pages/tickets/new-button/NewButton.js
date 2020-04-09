c2dae7297d47e7fe41c2c6b2680193cfa9977c65
Arnau Comas Codina <comas1992@gmail.com>
2020-04-09 20:55:55 +0200

    [feat] added link to Add button

--- react-app/src/pages/tickets/new-button/NewButton.js
+++ react-app/src/pages/tickets/new-button/NewButton.js
@@ -3,14 +3,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 
+import { Link } from "react-router-dom";
+
 const FloatingNewButton = () => {
 
   return (
     <div className="floating-new-button">
-      <Fab variant="extended" color="primary">
-        <AddIcon />
-        Nova necessitat
-      </Fab>
+      <Link to="/publicar" style={{ color: 'inherit', textDecoration: 'none' }}>
+        <Fab variant="extended" color="primary">
+          <AddIcon />
+          Nova necessitat
+        </Fab>
+      </Link>
     </div>
   );
 }
